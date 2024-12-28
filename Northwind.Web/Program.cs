e0cc21b90f2424dfd765e36591e6b5d4d1496ab7
Rares <>
2024-12-29 00:13:37 +0200

    Add custom middleware delegate

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -14,6 +14,29 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+    if (rep is not null)
+    {
+        WriteLine($"Endpont name: {rep.DisplayName}");
+        WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+    }
+
+    if (context.Request.Path == "/bonjour")
+    {
+        // If endpoint path matches
+        // This middleware becomes a terminating delegate returning immediatley 
+        await context.Response.WriteAsync("Bonjour Monde!");
+        return;
+    }
+
+    await next();
+
+    // Here we can modify the response after calling the next delegate
+});
+
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
